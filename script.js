a60f19ca3cba2c17bf281e8208fa21a6dfd9de29
Esther Gar <bamgboseesther3@gmail.com>
2024-07-30 23:18:53 +0100

    update

--- script.js
+++ script.js
@@ -2,6 +2,22 @@ const menuToggle = document.querySelector(".navLinks");
 const menuIcon = document.querySelector("#open");
 const closeIcon = document.querySelector("#close");
 
+const checkScreenWidth = function() {
+    if (window.innerWidth <= 768) {
+        menuToggle.style.display = "none";
+        menuIcon.style.display = "inline-block";
+    } else {
+        menuToggle.style.display = "flex";
+        menuIcon.style.display = "none";
+        closeIcon.style.display = "none";
+    }
+};
+
+
+const unLoad = function() {
+    checkScreenWidth();
+}
+
 menuIcon.addEventListener('click', function() {
     menuToggle.style.display = "flex";
     closeIcon.style.display = "inline-block";
@@ -14,3 +30,7 @@ const closeNav = function() {
     menuIcon.style.display = "inline-block";
 }
 closeIcon.addEventListener('click', closeNav);
+
+window.addEventListener('load', unLoad);
+
+window.addEventListener('resize', checkScreenWidth);
